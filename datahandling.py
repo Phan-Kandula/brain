519f6329bac22ac914a5796d8ad9d3fd86a5e411
phani1615 <phanindra.kandula@gmail.com>
2017-08-19 22:22:26 -0700

    trial

--- datahandling.py
+++ datahandling.py
@@ -2,5 +2,6 @@ import csv
 import numpy as np
 import serial
 
-PORT = "" #COM_
-ser = serial.Serial(PORT, 9600, timeout=None) #comment
+PORT = "" #COM comment_
+ser = serial.Serial(PORT, 9600, timeout=None)
+
